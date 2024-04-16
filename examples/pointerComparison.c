7308bc65e9b67593a260f58224a9d2d02541db2c
Isaac Barbosa <prospektsmarch22@DESKTOP-U0S23PD>
2024-04-16 22:15:07 +0100

    Adds examples folder, adds pointerComparison.c in the examples folder.

---
+++ examples/pointerComparison.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+//This is a example of pointer comparison, a third property of pointer arithmetic.
+
+int main(){
+    int arr[] = {10, 20, 30, 40, 50};
+    int *ptr1 = arr + 1;
+    int *ptr2 = arr + 3;
+
+    if (ptr1 > ptr2){
+        printf("ptr1 is after ptr2\n");
+    } else if (ptr1 < ptr2){
+        printf("ptr2 is after ptr1\n");
+    } else {
+        printf("ptr1 and ptr2 points to the same location\n");
+    }
+    return 0;
+}
